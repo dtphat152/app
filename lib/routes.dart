dc44c62e5b4dab6835a9e70b6063125d08d14e37
dtphat152 <phatb2014772@student.ctu.edu.vn>
2024-05-16 08:19:02 +0700

    first commit

---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+
+import 'package:app/ui/danh_muc/loai_vang.dart';
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  loaiVang.routeName: (ctx) => loaiVang(),
+};
+
